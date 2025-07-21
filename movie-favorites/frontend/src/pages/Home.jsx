5cb0b6e7a49c952b05339ce6bf2cdaf477fd5326
avirani1204 <avirani3@illinois.edu>
2025-07-21 11:23:14 -0500

    added search functionaliy (use state to conditionally render this movie card only if beginning of movie title begins with movie text

--- movie-favorites/frontend/src/pages/Home.jsx
+++ movie-favorites/frontend/src/pages/Home.jsx
@@ -1,18 +1,24 @@
 import MovieCard from "../components /MovieCard"
+import {useState} from "react"
 
 //contains entire user interface for home page
 function Home () {
     //array of diff movies and renders them dynamically 
 
+    //define piece of state that handles logic/state thats happening in our component
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         {id: 1, title: "John Wick", release_date: "2020" },
         {id: 2, title: "Terminator", release_date: "1999" },
         {id: 3, title: "The Matrix", release_date: "1998" },
-    ]
+    ];
 
-    const handleSearch = () => {
-
-    }
+    const handleSearch = (e) => {
+        e.preventDefault() // makes it so what u type in the search box doesnt disappear everytime u hit search 
+        alert(searchQuery)
+        setSearchQuery("")
+    };
     //display movies in rows?
 
     //.map function iterate over all values inside movies array
@@ -25,12 +31,17 @@ function Home () {
             <input type="text" 
             placeholder="Search for movies..." 
             className="search-input"
+            value= {searchQuery}
+            onChange = {(e) => setSearchQuery(e.target.value)}
             />
             <button type="submit" className="search-button"> Search</button>
         </form>
         <div className="movies-grid">
+            {/* use .map to search */}
             {movies.map((movie) => (
-                <MovieCard movie={movie} key={movie.id}/>
+                movie.title.toLowerCase().startsWith(searchQuery) && (
+                    <MovieCard movie={movie} key={movie.id}/>
+                )
             ))}
         </div>
     </div>
