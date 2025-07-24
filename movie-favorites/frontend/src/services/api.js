e040c6eb8a38fdd399f1d11532b2fd60bd413155
avirani1204 <avirani3@illinois.edu>
2025-07-24 13:24:14 -0500

    calling apis

---
+++ movie-favorites/frontend/src/services/api.js
@@ -0,0 +1,15 @@
+//separate file for all api calls
+
+const API_KEY = "41cd61d216f257268cfea8c4bdf9b80e"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+//most popular movies
+
+export const getPopularMovies = async () => {
+    //request in here
+    // use fetch function to send network request
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+    
+}
