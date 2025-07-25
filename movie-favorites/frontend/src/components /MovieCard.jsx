588e03d451525d1474ec461bb74a375d93baf2df
avirani1204 <avirani3@illinois.edu>
2025-07-25 10:47:54 -0500

    added button functionality for favoriting

--- movie-favorites/frontend/src/components /MovieCard.jsx
+++ movie-favorites/frontend/src/components /MovieCard.jsx
@@ -1,18 +1,25 @@
 //movie card component
 import "../css/MovieCard.css"
+import { useMovieContext } from "../contexts/MovieContext"
+
 function MovieCard({movie}){
     //func triggered when we click button
+    const {isFavorite, addToFavorites, removeFromFavorites} = useMovieContext()
+    const favorite = isFavorite(movie.id) // if we r currently favorited or not
 
-    function onFavoriteClick(){
-        
-
+    function onFavoriteClick(e){
+        e.preventDefault()
+        // if we r currently favorited -> unfavorite it 
+        if (favorite) removeFromFavorites(movie.id)
+        // but if we r not currently favorited -> favorite it 
+        else addToFavorites(movie)
     }
 
     return <div className="movie-card">
         <div className="movie-poster">
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
             <div className="movie-overlay">
-                <button className="favorite-btn" onClick={onFavoriteClick}>
+                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
                     ♥
                 </button>
             </div>
